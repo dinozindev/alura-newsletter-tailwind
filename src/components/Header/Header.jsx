2db486b66a2df8258af95520ce781d3711d51402
dinozindev <pugplayzcraft@gmail.com>
2023-11-06 17:11:38 -0300

    instalando o tailwind

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return <div>
+        <span>Olá, usuário</span>
+        <h1>Alura Newsletter</h1>
+        <input type="checkbox" />
+    </div>
+}
+
+export default Header
