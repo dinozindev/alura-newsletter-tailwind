183d5654d93d69832cb2c757fd3cae35c7dca076
dinozindev <pugplayzcraft@gmail.com>
2023-11-06 18:51:46 -0300

    finalizando aula 1. Adicionando componente de cabecalho

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
-    return <div>
-        <span>Olá, usuário</span>
-        <h1>Alura Newsletter</h1>
-        <input type="checkbox" />
+    return <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+        <span className="text-gray-100">Olá, usuário</span>
+        <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
+        <input className="hidden sm:block"type="checkbox" />
     </div>
 }
 
