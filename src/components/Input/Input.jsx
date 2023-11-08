007906cbc741662b8b940e48063939cffae99e12
dinozindev <pugplayzcraft@gmail.com>
2023-11-08 18:51:46 -0300

    aprimorando os inputs e botao

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,5 @@
+const Input = ({placeholder}) => {
+    return <input className="py-1 pl-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none" placeholder={placeholder} />
+}
+
+export default Input;
