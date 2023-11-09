328b954c32ba10859049c5dcadd137ae0059219d
dinozindev <pugplayzcraft@gmail.com>
2023-11-09 17:44:22 -0300

    finalizando aula 5. Adicionando line-clamp para os artigos e otimizando a aplicação para o ambiente de produção com cssnano

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -3,12 +3,12 @@ const Article = ({ title, text, tags, image, alt }) => {
         <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">{title}</h3>
         <div className="w-full sm:flex justify-end gap-2 pr-5 hidden">
             {
-                tags.map((tag) => <span className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 dark:text-gray-300 font-bold text-xs uppercase hover:bg-alura-200 hover:scale-110" key={tag}>{tag}</span> )
+                tags.map((tag) => <span className="alura-tag" key={tag}>{tag}</span> )
             }
         </div>
         <div className="gap-1 grid">
             {
-                text.map((content, index) => <p className="text-alura-200 dark:text-gray-300" key={index}>{content}</p>)
+                text.map((content, index) => <p className="text-alura-200 dark:text-gray-300 line-clamp-6 sm:line-clamp-none" key={index}>{content}</p>)
             }
         </div>
         {image && <img className="sm:p-4" src={image}/> }
