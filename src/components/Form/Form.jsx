fbe1a0c2ed6f44a3e9516cee2d8be25e071c27a6
dinozindev <pugplayzcraft@gmail.com>
2023-11-09 16:57:00 -0300

    continuando aula 5. Refatorando componentes, utilizando as camadas de base e components do Tailwind para componentes reutilizaveis e estilizacoes base

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,3 @@
-import Input from "../Input/Input";
 
 const Form = ({onSubmit}) => {
     const safeSubmit = e => {
@@ -9,9 +8,9 @@ const Form = ({onSubmit}) => {
         onSubmit({name, email})
     }
     return <form onSubmit={safeSubmit} className="flex flex-col items-center justify-center h-full gap-10 -mt-20 mx-5">
-        <Input type="text" placeholder="Insira seu nome..." />
-        <Input type="text" placeholder="Insira seu email..." />
-        <button type="submit" className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none hover:animate-pulse hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black">Seguir</button>
+        <input required className="alura-input" type="text" placeholder="Insira seu nome..." />
+        <input required className="alura-input" type="text" placeholder="Insira seu email..." />
+        <button type="submit" className="alura-button">Seguir</button>
     </form>
 }
 
